5d5033ff0f969b4fbd9bbe90afa4b06073d51a1d
Adonias Goes <adoniasgoes86@gmail.com>
2026-03-27 13:42:01 -0300

    new version

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -5,6 +5,7 @@ import Project01 from "../assets/Project01.jpeg"
 import Project02 from "../assets/Project02.jpeg"
 import Project03 from "../assets/Project03.jpeg"
 import Project04 from "../assets/Project04.jpeg"
+import Project05 from "../assets/Project05.jpeg"
 const navItems = [
     {
        label: "Home",
@@ -78,7 +79,7 @@ const projects = [
         github: "https://github.com/adoniasgoesw/iPhone-17"
     },
 
-    {   image: Project01,
+    {   image: Project02,
         name: "Layers – The Design Flow",
         desc: "A modern landing page for Layers, a powerful design tool that lets you create, prototype, and collaborate effortlessly. Combine simplicity with advanced features to stay in your creative flow.",
         skills: ["React", "Tailwindcss"],
@@ -87,7 +88,7 @@ const projects = [
     },
 
     {
-        image: Project01,
+        image: Project03,
         name: "FlorexTrade – Next-Gen Forex",
         desc: "FlorexTrade is a modern forex trading platform designed for both novice and professional traders. With real-time analytics, institutional-grade security, and advanced trading tools, users can trade confidently and efficiently.",
         skills: ["React", "Tailwindcss"],
@@ -96,12 +97,21 @@ const projects = [
     },
 
     {
-        image: Project01,
+        image: Project04,
         name: "RPG Dev Portfolio",
         desc: "Embark on a developer’s quest: a front-end portfolio styled like an RPG adventure, where each project is a mission, every skill is a power unlocked, and the UI blends magic with modern web technologies.",
         skills: ["React", "Tailwindcss"],
         link:"https://adoniasgoes.netlify.app/",
         github: "https://github.com/adoniasgoesw/Portfolio/tree/main"
+    },
+
+    {
+        image: Project05, // substitua pela imagem do Aurora Pro
+        name: "Aurora Pro",
+        desc: "Aurora Pro is a Chrome extension and modern landing page that automates your Meta AI workflow. Generate images and animations in batches, monitor tasks in real time, and keep your files organized—boosting your creative flow with minimal manual effort.",
+        skills: ["React", "Tailwindcss", "Chrome Extension", "Meta AI"],
+        link: "https://aurorapro.netlify.app/",
+        github: "https://github.com/adoniasgoesw/Aurora"
     }
 ]
 export { navItems , skills, projects};
